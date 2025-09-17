e25bd4e52dc103cddbfbfb1d3250bdcf1105cfce
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-09-17 02:27:48 -0400

    added empty pages for Calendar and Inbox

---
+++ app/(Kambaz)/Calendar/page.tsx
@@ -0,0 +1,7 @@
+export default function Lab2() {
+  return (
+    <div>
+      <h2>Calendar</h2>
+    </div>
+  );
+}
---
+++ app/(Kambaz)/Inbox/page.tsx
@@ -0,0 +1,7 @@
+export default function Lab2() {
+  return (
+    <div>
+      <h2>Inbox</h2>
+    </div>
+  );
+}
