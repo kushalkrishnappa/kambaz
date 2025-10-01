3aebff0b1afc63c6a168682d5e47db18196582cc
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-10-01 16:27:33 -0400

    - Styling Addons with React Bootstrap

--- app/Labs/Lab2/BootstrapForms.tsx
+++ app/Labs/Lab2/BootstrapForms.tsx
@@ -1,5 +1,6 @@
-import { FormCheck, FormControl, FormLabel, FormSelect } from "react-bootstrap";
+import { FormCheck, FormControl, FormLabel, FormSelect, InputGroup } from "react-bootstrap";
 import FormRange from "react-bootstrap/esm/FormRange";
+import InputGroupText from "react-bootstrap/esm/InputGroupText";
 
 export default function BootstrapForms() {
   return (
@@ -48,11 +49,25 @@ export default function BootstrapForms() {
           disabled
         />
       </div>
-      
+
       <div id="wd-css-styling-range-and-sliders">
         <h3>Range</h3>
         <FormLabel>Example range</FormLabel>
         <FormRange min="0" max="5" step="0.5" />
+      </div>
+
+      <div id="wd-css-styling-addons">
+        <h3>Addons</h3>
+        <InputGroup className="mb-3">
+          <InputGroupText>$</InputGroupText>
+          <InputGroupText>0.00</InputGroupText>
+          <FormControl />
+        </InputGroup>
+        <InputGroup>
+          <FormControl />
+          <InputGroupText>$</InputGroupText>
+          <InputGroupText>0.00</InputGroupText>
+        </InputGroup>
       </div>
     </div>
   );
