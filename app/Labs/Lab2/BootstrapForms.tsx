3133b6f8a61bba65f0c023f7644c41090a5915b3
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-10-01 16:32:04 -0400

    - Styling Responsive Forms with React Bootstrap

--- app/Labs/Lab2/BootstrapForms.tsx
+++ app/Labs/Lab2/BootstrapForms.tsx
@@ -1,5 +1,7 @@
 import {
-    Col,
+    Button,
+  Col,
+  Form,
   FormCheck,
   FormControl,
   FormLabel,
@@ -107,6 +109,67 @@ export default function BootstrapForms() {
             <FormControl as="textarea" style={{ height: "100px" }} />
           </Col>
         </Row>
+
+        <div id="wd-css-responsive-forms-2">
+          <h3>Responsive forms 2</h3>
+          <Form>
+            <Row className="mb-3" controlId="formHorizontalEmail">
+              <FormLabel column sm={2}>
+                {" "}
+                Email{" "}
+              </FormLabel>
+              <Col sm={10}>
+                {" "}
+                <FormControl type="email" placeholder="Email" />{" "}
+              </Col>
+            </Row>
+            <Row className="mb-3" controlId="formHorizontalPassword">
+              <FormLabel column sm={2}>
+                {" "}
+                Password{" "}
+              </FormLabel>
+              <Col sm={10}>
+                {" "}
+                <FormControl type="password" placeholder="Password" />{" "}
+              </Col>
+            </Row>
+            <fieldset>
+              <Row className="mb-3">
+                <FormLabel as="legend" column sm={2}>
+                  {" "}
+                  Radios{" "}
+                </FormLabel>
+                <Col sm={10}>
+                  <FormCheck
+                    type="radio"
+                    label="First radio"
+                    name="formHorizontalRadios"
+                    defaultChecked
+                  />
+                  <FormCheck
+                    type="radio"
+                    label="Second radio"
+                    name="formHorizontalRadios"
+                  />
+                  <FormCheck
+                    type="radio"
+                    label="Third radio"
+                    name="formHorizontalRadios"
+                  />
+                  <FormCheck
+                    type="radio"
+                    label="Remember me"
+                    name="formHorizontalRadios"
+                  />
+                </Col>
+              </Row>
+            </fieldset>
+            <Col>
+              {" "}
+              <Button type="submit">Sign in</Button>{" "}
+            </Col>
+          </Form>
+        </div>
       </div>
     </div>
   );
