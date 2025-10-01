4b225a4f931bc47e57437cb8db880717b6786edf
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-10-01 16:40:10 -0400

    - nit fix

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,36 +1,3 @@
-// import Link from "next/link";
-// export default function TOC() {
-//   return (
-//     <ul>
-//       <li>
-//         <Link href="/Labs" id="wd-lab1-link">
-//           Home
-//         </Link>
-//       </li>
-//       <li>
-//         <Link href="/Labs/Lab1" id="wd-lab1-link">
-//           Lab 1
-//         </Link>
-//       </li>
-//       <li>
-//         <Link href="/Labs/Lab2" id="wd-lab2-link">
-//           Lab 2
-//         </Link>
-//       </li>
-//       <li>
-//         <Link href="/Labs/Lab3" id="wd-lab3-link">
-//           Lab 3
-//         </Link>
-//       </li>
-//       <li>
-//         <Link href="/" id="wd-lab3-link">
-//           Kambaz
-//         </Link>
-//       </li>
-//     </ul>
-//   );
-// }
-
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
 export default function TOC() {
@@ -56,4 +23,3 @@ export default function TOC() {
      </NavItem>
    </Nav>
 );}
-
