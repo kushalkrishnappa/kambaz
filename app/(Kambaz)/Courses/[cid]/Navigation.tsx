bd5f00a0af3881910d35a70ba1220bf59002e0a9
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-10-01 20:53:49 -0400

    fix for courses navigation - uses the path mapping to highlight the component

--- app/(Kambaz)/Courses/[cid]/Navigation.tsx
+++ app/(Kambaz)/Courses/[cid]/Navigation.tsx
@@ -1,20 +1,35 @@
+"use client";
 import Link from "next/link";
+import { usePathname, useParams } from "next/navigation";
+
 export default function CourseNavigation() {
+  const pathname = usePathname();
+  const { cid } = useParams();
+
+  const links = [
+    { label: "Home", path: `/Courses/${cid}/Home` },
+    { label: "Modules", path: `/Courses/${cid}/Modules` },
+    { label: "Piazza", path: `/Courses/${cid}/Piazza` },
+    { label: "Zoom", path: `/Courses/${cid}/Zoom` },
+    { label: "Assignments", path: `/Courses/${cid}/Assignments` },
+    { label: "Quizzes", path: `/Courses/${cid}/Quizzes` },
+    { label: "People", path: `/Courses/${cid}/People/Table` },
+  ];
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link href="/Courses/1234/Home" id="wd-course-home-link"
-        className="list-group-item active border-0"> Home </Link>
-      <Link href="/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border-0"> Modules </Link>
-      <Link href="/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border-0"> Piazza </Link>
-      <Link href="/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border-0"> Zoom </Link>
-      <Link href="/Courses/1234/Assignments" id="wd-course-assignments-link"
-        className="list-group-item text-danger border-0"> Assignments </Link>
-      <Link href="/Courses/1234/Quizzes" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border-0"> Quizzes </Link>
-      <Link href="/Courses/1234/People/Table" id="wd-course-people-link"
-        className="list-group-item text-danger border-0" > People </Link>
+      {links.map((link) => (
+        <Link
+          key={link.path}
+          href={link.path}
+          id={`wd-course-${link.label.toLowerCase()}-link`}
+          className={`list-group-item border-0 ${
+            pathname.includes(link.path) ? "active" : "text-danger"
+          }`}
+        >
+          {link.label}
+        </Link>
+      ))}
     </div>
-);}
+  );
+}
