c8dc5eea016f44fe86b5676c32df530715961f02
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-10-01 22:48:36 -0400

    added id to github link

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -19,7 +19,7 @@ export default function TOC() {
        <NavLink href="/" as={Link}>Kambaz</NavLink>
      </NavItem>
      <NavItem>
-       <NavLink href="https://github.com/kushalkrishnappa">My GitHub</NavLink>
+       <NavLink href="https://github.com/kushalkrishnappa" id="wd-github">My GitHub</NavLink>
      </NavItem>
    </Nav>
 );}
