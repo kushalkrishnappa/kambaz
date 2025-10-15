e2eb49767c6285d2bf6fecddadb8a06360012a20
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-10-15 00:05:55 -0400

    Lab3: - LegacyFunctions - ArrowFunctions - ImpliedReturn - TemplateLiterals - SimpleArrays - ArrayIndexAndLength - AddingAndRemovingToFromArrays - ForLoops - MapFunction

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -5,6 +5,15 @@ import IfElse from "./IfElse";
 import TernaryOperator from "./TernaryOperator";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ForLoops from "./ForLoops";
+import MapFunction from "./MapFunction";
 
 export default function Lab3() {
   return (
@@ -17,6 +26,20 @@ export default function Lab3() {
       <TernaryOperator />
       <ConditionalOutputIfElse />
       <ConditionalOutputInline />
+
+      {/* Functions */}
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+
+      {/* Arrays */}
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+
     </div>
   );
 }
